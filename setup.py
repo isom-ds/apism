361df85caed1e7595788be4cf35be93d713cbb39
briceshun <bricevalentinshun@gmail.com>
2024-09-15 10:03:40 +0000

    convert to package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uoa_isom_ds_sm_api_sdk',
+    version='0.0.1',
+    description='Simplified social media API calls',
+    url='https://github.com/isom-ds/social-media-api-sdk',
+    author='Brice Shun',
+    author_email='brice.kok.shun@auckland.ac.nz',
+    license='internal_use',
+    packages=find_packages(),
+    zip_safe=False
+)
