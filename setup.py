6532191482d31d716da279a075df02df44c00b3b
briceshun <bricevalentinshun@gmail.com>
2024-09-26 21:52:27 +0000

    try auto install of required packages

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+# Read the requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='uoa_isom_ds_sm_api_sdk',
-    version='0.0.15',
+    version='0.0.16',
     description='Simplified social media API calls',
     url='https://github.com/isom-ds/social-media-api-sdk',
     author='Brice Shun',
     author_email='brice.kok.shun@auckland.ac.nz',
     license='internal_use',
     packages=find_packages(),
+    install_requires=requirements,
     zip_safe=False
 )
