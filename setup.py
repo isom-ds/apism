02384bb51d952dd36b54bf50e846ace9f87cea16
briceshun <bricevalentinshun@gmail.com>
2024-10-29 22:10:14 +1300

    return none if transcript parser error

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='apism',
-    version='0.0.11',
+    version='0.0.12',
     description='Simplified social media API calls',
     url='https://github.com/isom-ds/apism',
     author='Brice Shun',
