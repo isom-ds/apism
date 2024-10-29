fc6155bbf89fa3cab00279ab1c3835db2415eb8d
briceshun <bricevalentinshun@gmail.com>
2024-10-29 21:57:58 +1300

    upgrade save_as JSON and CSV

--- apism/youtube/save_as.py
+++ apism/youtube/save_as.py
@@ -5,45 +5,15 @@ import os
 import re
 import warnings
 
-def to_json(results, file_path=None):
+def _process_for_save(results, default_cols, shorten_cols):
     """
-    Save the search results to a file in JSON format.
-
-    Args:
-        data (dict): The search results data to save.
-        file_path (str): The path where the files will be saved.
-    """
-    # Determine file path
-    if file_path is None:
-        output_path = os.path.join(os.getcwd(), 'results.json')
-    else:
-        output_path = os.path.join(file_path, 'results.json')
-
-    with open(output_path, 'w') as json_file:
-        json.dump(results, json_file)
-
-def to_csv(results, file_path=None, **kwargs):
-    """
-    Save the search results to files in CSV format.
+    Process results for save.
 
     Args:
         data (dict): The results data to save.
-        file_path (str): The path where the files will be saved.
-    Kwargs:
-        default_cols (bool): Use default column names. Default=False
-        shorten_cols (bool): Shorten column names. Default=False
-        force_output (bool): Force output even if no data is available. Default=False
-        verbose (bool): Print verbose output. Default=False
+        default_cols (bool): Use default column names.
+        shorten_cols (bool): Shorten column names.
     """
-    # Kwargs
-    default_cols = kwargs.get('default_cols', False)
-    shorten_cols = kwargs.get('shorten_cols', False)
-    force_output = kwargs.get('force_output', False)
-    verbose      = kwargs.get('verbose', False)
-
-    # Determine file path
-    if file_path is None:
-        file_path = os.getcwd()
 
     # Flatten dictionary
     flattened = _flatten_results(results)
@@ -77,7 +47,74 @@ def to_csv(results, file_path=None, **kwargs):
 
         # Reorder dict
         output[k] = _reorder_dict(flattened[k], col_names[k])
-        
+
+    return output, col_names
+
+def to_json(results, file_path=None, **kwargs):
+    """
+    Save the search results to files in JSON format.
+
+    Args:
+        data (dict): The results data to save.
+        file_path (str): The path where the files will be saved.
+    Kwargs:
+        default_cols (bool): Use default column names. Default=False
+        shorten_cols (bool): Shorten column names. Default=False
+        force_output (bool): Force output even if no data is available. Default=False
+        verbose (bool): Print verbose output. Default=False
+    """
+    # Kwargs
+    default_cols = kwargs.get('default_cols', False)
+    shorten_cols = kwargs.get('shorten_cols', False)
+    force_output = kwargs.get('force_output', False)
+    verbose      = kwargs.get('verbose', False)
+
+    # Determine file path
+    if file_path is None:
+        file_path = os.getcwd()
+
+    # Process
+    output, __ = _process_for_save(results, default_cols, shorten_cols)
+
+    # Return
+    for k in output.keys():
+        # Raise warning if no data is available
+        if (output[k] is None or len(output[k]) == 0) and verbose:
+            warnings.warn(f"No {k} data available.")
+
+        # Write data to JSON files
+        if output[k] or force_output:
+            with open(os.path.join(file_path, f"{k}.json"), 'w') as json_file:
+                json.dump(output[k], json_file)
+
+def to_csv(results, file_path=None, **kwargs):
+    """
+    Save the search results to files in CSV format.
+
+    Args:
+        data (dict): The results data to save.
+        file_path (str): The path where the files will be saved.
+    Kwargs:
+        default_cols (bool): Use default column names. Default=False
+        shorten_cols (bool): Shorten column names. Default=False
+        force_output (bool): Force output even if no data is available. Default=False
+        verbose (bool): Print verbose output. Default=False
+    """
+    # Kwargs
+    default_cols = kwargs.get('default_cols', False)
+    shorten_cols = kwargs.get('shorten_cols', False)
+    force_output = kwargs.get('force_output', False)
+    verbose      = kwargs.get('verbose', False)
+
+    # Determine file path
+    if file_path is None:
+        file_path = os.getcwd()
+
+    # Process
+    output, col_names = _process_for_save(results, default_cols, shorten_cols)
+
+    # Return
+    for k in output.keys():        
         # Raise warning if no data is available
         if (output[k] is None or len(output[k]) == 0) and verbose:
             warnings.warn(f"No {k} data available.")
